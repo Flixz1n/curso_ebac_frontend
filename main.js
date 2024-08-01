ddc7c2135ec0dcbccd3d98e621847b658b482c8a
Flixz1n <121522163+Flixz1n@users.noreply.github.com>
2024-08-01 17:58:08 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const username = 'Flixz1n';
+const url = `https://api.github.com/users/${username}`;
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+    document.getElementById('avatar').src = data.avatar_url;
+    document.getElementById('name').textContent = data.name;
+    document.getElementById('followers').textContent = data.followers;
+    document.getElementById('following').textContent = data.following;
+    document.getElementById('repos').textContent = data.public_repos;
+    document.getElementById('link').href = profileUrl;
+})
+.catch(error => {
+    console.error('Erro ao buscar dados:', error);
+});
