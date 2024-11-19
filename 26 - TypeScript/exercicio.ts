098684dd861069f26681accf378d3c76ff7f4499
Flixz1n <121522163+Flixz1n@users.noreply.github.com>
2024-11-19 18:34:56 -0300

    Add files via upload

---
+++ 26 - TypeScript/exercicio.ts
@@ -0,0 +1,15 @@
+
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+function saudar(nome: string): string {
+    return `Olá ${nome}`;
+}
+/////////////////////////////////////////////////////////
+
+const resultadoMultiplicacao = multiplicar(3, 5);
+console.log(resultadoMultiplicacao); // Saída: 15
+
+const saudacao = saudar("Maria");
+console.log(saudacao); 
